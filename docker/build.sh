7bd6903266feb5ad05aba2e29df0babf720703fc
koustubh1012 <koustubh1012@gmail.com>
2025-05-15 14:52:32 -0400

    added build and run scripts

---
+++ docker/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t voxl-ros2-humble -f voxl_ros2_humble.Dockerfile .
---
+++ docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --rm   --user $(id -u):$(id -g)   --network=host   --ipc=host   --pid=host   -e DISPLAY=$DISPLAY   -e ROS_DOMAIN_ID=0   -e ROS_LOCALHOST_ONLY=0   -v /tmp/.X11-unix:/tmp/.X11-unix   --name voxl_humble   voxl-ros2-humble:latest
