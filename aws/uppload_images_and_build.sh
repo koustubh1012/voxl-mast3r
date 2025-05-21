c2d503362ff31c1daee6a524d0e3d274b7f9dea1
koustubh1012 <koustubh1012@gmail.com>
2025-05-20 23:22:02 -0400

    Modified IP

--- aws/uppload_images_and_build.sh
+++ aws/uppload_images_and_build.sh
@@ -2,9 +2,9 @@
 IMAGES_DIR=/home/user/images
 ZIP_NAME=images_$(date +%Y%m%d_%H%M%S).tar.gz
 EC2_USER=ubuntu
-EC2_IP=54.227.65.94
+EC2_IP=xx.xxx.xxx.xxx # Replace with your EC2 instance's public IP
 
-chmod 400 aerial-rob.pem
+chmod 400 aerial-rob.pem # Generate your own key pair and replace this with the path to your key
 
 echo "[+] Zipping images..."
 tar -czvf $ZIP_NAME -C $IMAGES_DIR .
